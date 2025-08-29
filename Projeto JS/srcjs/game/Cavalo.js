9dc1628b2f0e864e054b439c04533a51f7204830
unknown <arthur.barreto@dcomp.ufs.br>
2025-08-29 12:53:22 -0300

    Criação da Pasta JS

---
+++ Projeto JS/srcjs/game/Cavalo.js
@@ -0,0 +1,31 @@
+import { Peca } from './Peca.js';
+
+export class Cavalo extends Peca {
+    constructor(cor) {
+
+        super(cor); 
+        this.nomePeca = "Cavalo";
+    }
+
+   
+    isMovimentoValido(tabuleiro, linhaInicial, colunaInicial, linhaFinal, colunaFinal) {
+       
+        const pecaDestino = tabuleiro.getPeca(linhaFinal, colunaFinal);
+
+        if (pecaDestino !== null && pecaDestino.getCor() === this.getCor()) {
+            return false;
+        }
+
+        // A lógica matemática e de booleanos é exatamente a mesma.
+        const diffLinha = Math.abs(linhaFinal - linhaInicial);
+        const diffColuna = Math.abs(colunaFinal - colunaInicial);
+
+        const ehMovimentoL = (diffLinha === 2 && diffColuna === 1) || (diffLinha === 1 && diffColuna === 2);
+
+        return ehMovimentoL;
+    }
+
+    toString() {
+        return "C{}";
+    }
+}
