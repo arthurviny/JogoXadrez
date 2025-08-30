98226cda7584faea80058005a2fb372e3394bb76
unknown <arthur.barreto@dcomp.ufs.br>
2025-08-29 22:41:35 -0300

    Tabuleiro e Ajustes

---
+++ Projeto JS/srcjs/usuario/GameController.js
@@ -0,0 +1,11 @@
+import { Tabuleiro } from '../game/Tabuleiro.js';
+import { Peca } from '../game/Peca.js';
+import { Peao } from '../game/Peao.js';
+import { BoboDaCorte } from '../game/BoboDaCorte.js';
+import { Ladrao } from '../game/Ladrao.js';
+
+// Classe principal que controla a interface e o fluxo do jogo
+export class GameController {
+
+        
+}
